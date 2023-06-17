1c021607609766003ef79d84c562c4addb6162d8
avinash8121612104 <136907225+avinash8121612104@users.noreply.github.com>
2023-06-17 19:22:30 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('my parents new healthy dinner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale, spinach & rocket smoothie')
+streamlit.text('hard-boiled free-range egg')
